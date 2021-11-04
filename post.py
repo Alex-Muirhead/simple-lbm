1a827da6972ba4f0a1bae117ab9e7f9a2d6fa756
Alexander Muirhead <s4435062@fac-login-1.local>
2021-11-04 22:39:24 +1000

    Smooth out post processing

--- post.py
+++ post.py
@@ -9,6 +9,11 @@ with h5py.File('output.h5') as file:
     vel_y = np.array(file['vel_y'])
     vel   = np.dstack([vel_x, vel_y])
 
+with h5py.File('taylor_green.h5') as file:
+    group = file['layout']
+    timesteps = int(group.attrs['timesteps'])
+    savestep = int(group.attrs['savestep'])
+
 
 figsize = plt.figaspect(1)
 fig, ax = plt.subplots(dpi=250, figsize=figsize)
@@ -19,25 +24,27 @@ ax.set(adjustable="datalim")
 plt.savefig('densitymap.png')
 
 
-iters = rho.shape[0] - 1
+def prediction(t):
+    return 0.2*np.exp(-2*2.3E-04*t)
+
 
 vel_norm  = np.sqrt(vel_x**2 + vel_y**2)
 max_vel   = np.max(vel_norm, axis=(1, 2))
-timesteps = 100*np.arange(0, iters+1)
-predicted = 0.2*np.exp(-2*2.3E-04*timesteps)
-error     = np.abs(max_vel - predicted)
+recorded  = np.arange(0, timesteps+1, savestep)
+smooth_t  = np.linspace(0, timesteps, 500)
+error     = np.abs(max_vel - prediction(recorded))
 
 fig, (axData, axErr) = plt.subplots(
     2, 1, sharex=True, dpi=250,
     gridspec_kw={'height_ratios': [3, 1]},
     figsize=(3.5, 3.5)
 )
-axData.plot(timesteps, max_vel, label='Simulated')
-axData.plot(timesteps, predicted, label='Predicted')
+axData.plot(recorded, max_vel, label='Simulated', linestyle='', marker='*')
+axData.plot(smooth_t, prediction(smooth_t), label='Predicted', linestyle='--')
 axData.legend()
 axData.set_ylabel('Maximum velocity')
-axErr.semilogy(timesteps, error*100, 'r')
-axErr.set_xlim(0, 100*iters)
+axErr.semilogy(recorded, error*100, linestyle='', marker='+', color='r')
+axErr.set_xlim(0, timesteps)
 axErr.set_ylim(1E-02, 1)
 axErr.set_xlabel('Timestep')
 axErr.set_ylabel(r'Error (\%)')
